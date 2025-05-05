7f7c734d6f1abb24c30712d2b449ff5514490b1f
HienaDev <ant.rodrigues1999@gmail.com>
2025-05-05 15:15:07 +0100

    Add bopping to columns

--- Assets/DJBoss.cs
+++ Assets/DJBoss.cs
@@ -1,15 +1,54 @@
+using System;
 using UnityEngine;
+using static PhaseManager;
 
 public class DJBoss : MonoBehaviour, BossInterface
 {
+
+    [Serializable]
+    public struct Collumn
+    {
+        public GameObject collumn;
+        public int index;
+        public Transform firePoint;
+    }
+
+    [SerializeField] private int numberOfWeakspointsToDestroy = 2;
+    private int tutorialWeakpointsDestroyed = 0;
+
+    [SerializeField] private Collumn[] collumns;
+
     public void PhaseEnded()
     {
-        //throw new System.NotImplementedException();
+        
+    }
+
+    public void StartAttack(PhaseManager.SubPhase subphase)
+    {
+        Debug.Log(subphase);
+        //switch (subphase)
+        //{
+        //    case PhaseManager.SubPhase.Tutorial:
+        //        StartCoroutine(SpawnTutorialWeakpoints());
+        //        break;
+        //    case PhaseManager.SubPhase.Easy:
+        //        fightStarted = true;
+        //        break;
+        //    case PhaseManager.SubPhase.Normal:
+        //        NormalDifficulty();
+        //        fightStarted = true;
+        //        break;
+        //}
+    }
+
+    public void NormalDifficulty()
+    {
+
     }
 
     public void StartBoss(PhaseManager.SubPhase subPhase)
     {
-        //throw new System.NotImplementedException();
+        StartAttack(subPhase);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,5 +61,10 @@ public class DJBoss : MonoBehaviour, BossInterface
     void Update()
     {
         
+    }
+
+    private void SpawnCollumn(int pos)
+    {
+
     }
 }
---
+++ Assets/ScaleWithMusic.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class ScaleWithMusic : MonoBehaviour
+{
+    private Vector3 originalScale;
+    private Tween currentTween;
+
+    [SerializeField] private Vector3 scaleAmount = new Vector3(0.3f, 0.3f, 0.3f);
+    [SerializeField] private float duration = 0.4f;
+
+    void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        if (MoveWithMusic.Instance.bop)
+        {
+            Pulse(scaleAmount, duration);
+        }
+    }
+
+    public void Pulse(Vector3 scaleAmount, float duration)
+    {
+        currentTween?.Kill();
+
+        Vector3 targetScale = new Vector3(
+            originalScale.x + scaleAmount.x,
+            originalScale.y + scaleAmount.y,
+            originalScale.z + scaleAmount.z
+        );
+
+        float halfDuration = duration / 2f;
+
+        currentTween = transform.DOScale(targetScale, halfDuration)
+            .SetEase(Ease.InBounce)
+            .OnComplete(() =>
+                transform.DOScale(originalScale, halfDuration)
+                         .SetEase(Ease.OutBounce));
+    }
+}
